3e8702f932364eadcefe24b711fc4cd51812bf09
AniKingDJ <anirudhsbatate123@gmail.com>
2025-10-16 09:57:32 +0530

    Demo_react_program

---
+++ reactdemo/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+
+const app=()=>
+{
+  return(
+    <>
+    <Header />{" "}
+    <div>
+      <h1>Hello From Reacts!</h1>
+      <p>I am Anirudh S,From AIML</p>
+    </div>
+    </>
+  );
+}
+export default app;
